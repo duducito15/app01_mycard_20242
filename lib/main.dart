ec3cf684165b8d28c26c5159f77315bbee629031
Eduardo <echavezv@utea.edu.pe>
2024-12-12 07:57:17 -0500

    Terminado

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,76 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.w400,
               ),
             ),
+            Divider(
+              thickness: 0.7,
+              color: Colors.white,
+              indent: 70.0,
+              endIndent: 70.0,
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                horizontal: 14.0,
+                vertical: 8.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                title: Text("+51 983 717 546"),
+                subtitle: Text("Teléfono"),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                horizontal: 14.0,
+                vertical: 8.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.indigo,
+                ),
+                title: Text("eduardo.chavez@utea.edu.pe"),
+                subtitle: Text("Correo electrónico"),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("assets/images/facebook.png"),
+                  height: 60.0,
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Image(
+                  image: AssetImage("assets/images/twitter.png"),
+                  height: 60.0,
+                ),
+                SizedBox(
+                  width: 20.0,
+                ),
+                Image(
+                  image: AssetImage("assets/images/instagram.png"),
+                  height: 60.0,
+                ),
+              ],
+            ),
           ],
         ),
       ),
