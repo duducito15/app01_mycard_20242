06b9c9dcf5d3f0c4b0ae34ba1465e7bea2193f91
Eduardo <echavezv@utea.edu.pe>
2024-12-18 06:38:42 -0500

    MyCard App completo

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
